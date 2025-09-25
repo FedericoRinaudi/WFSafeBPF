0226c736c5f82cb3a5c73ea9aca1225f5f6d571b
FedericoRinaudi <federicorinaudi@gmail.com>
2025-09-25 14:29:51 +0200

    FIX: Checksums now working (skb checksum update commented, not nedeed here, but in some config may be)

--- eBPF/packet_dropper.bpf.c
+++ eBPF/packet_dropper.bpf.c
@@ -78,39 +78,107 @@ static __always_inline __u8 extract_from_packet(struct __sk_buff *skb, __u8 *ip_
     return 1;
 }
 
-
-/* Update IP and TCP checksums after packet length change */
-//static __always_inline __s8 update_len_and_checksums(struct __sk_buff *skb, __u8 ip_header_len, __u16 old_ip_len, __u16 new_ip_len)
+/* helper: delta csum per un campo 16-bit on-wire (usa buffer 32-bit) */
+//static __always_inline int csum_diff_16(__be16 old_be, __be16 new_be, __wsum *acc)
 //{
-//    __be16 new_ip_len_be = bpf_htons(new_ip_len);
-//    __be16 old_ip_len_be = bpf_htons(old_ip_len); 
-//    
-//    if (bpf_skb_store_bytes(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, tot_len),
-//                            &new_ip_len_be, sizeof(new_ip_len_be), BPF_F_RECOMPUTE_CSUM) < 0)
+//    __be32 old32 = (__be32)(((__u32)old_be) << 16);
+//    __be32 new32 = (__be32)(((__u32)new_be) << 16);
+//    __s64 t = bpf_csum_diff(&old32, sizeof(old32), &new32, sizeof(new32), *acc);
+//    if (t < 0)
 //        return -1;
-//    
-//    if (bpf_l3_csum_replace(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check),
-//                            old_ip_len_be, new_ip_len_be,
-//                            sizeof(__u16)) < 0)
-//        return -1;
-//
-//    __u16 zero = 0;
-//    if (bpf_skb_store_bytes(skb,
-//                            sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check),
-//                            &zero, sizeof(zero),
-//                            BPF_F_RECOMPUTE_CSUM) < 0)
-//        return -1;
-//
-//    //if (bpf_l4_csum_replace(skb, sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check),
-//    //                       old_ip_len - ip_header_len, new_ip_len - ip_header_len,
-//    //                       BPF_F_PSEUDO_HDR | sizeof(__u16)) < 0)
-//    //    return -1;   
-//    
+//    *acc = (__wsum)t;
 //    return 0;
 //}
 
+
+/* Update IP and TCP checksums after packet length change */
+static __always_inline __s8 update_len_and_checksums(struct __sk_buff *skb, __u8 ip_header_len, __u16 old_ip_len, __u16 new_ip_len, __wsum acc)
+{
+    __be16 new_ip_len_be = bpf_htons(new_ip_len);
+    __be16 old_ip_len_be = bpf_htons(old_ip_len); 
+
+    /* leggi IP checksum "old" on-wire */
+    //__be16 ip_csum_old_be;
+    //if (bpf_skb_load_bytes(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check), &ip_csum_old_be, sizeof(ip_csum_old_be)) < 0)
+    //    return -1;
+
+    //bpf_printk("[UPDATE] Old IP checksum (on-wire): 0x%04x", bpf_ntohs(ip_csum_old_be));
+    
+    if (bpf_skb_store_bytes(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, tot_len),
+                            &new_ip_len_be, sizeof(new_ip_len_be), 0) < 0)
+        return -1;
+
+    //__be16 ip_csum_should_be_old;
+    //if (bpf_skb_load_bytes(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check), &ip_csum_should_be_old, sizeof(ip_csum_should_be_old)) < 0)
+    //    return -1;
+
+    //bpf_printk("[UPDATE] Old IP checksum (on-wire): 0x%04x, should be: 0x%04x", bpf_ntohs(ip_csum_old_be), bpf_ntohs(ip_csum_should_be_old));
+
+    if (bpf_l3_csum_replace(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check),
+                            old_ip_len_be, new_ip_len_be,
+                            sizeof(__u16)) < 0)
+        return -1;
+
+    //__be16 ip_csum_new_be;
+    //if (bpf_skb_load_bytes(skb, sizeof(struct ethhdr) + offsetof(struct iphdr, check), &ip_csum_new_be, sizeof(ip_csum_new_be)) < 0)
+    //    return -1;
+
+    //bpf_printk("[UPDATE] New IP checksum: 0x%04x, should be different from old: 0x%04x", bpf_ntohs(ip_csum_new_be), bpf_ntohs(ip_csum_old_be));
+
+    __u16 old_tcp_len = old_ip_len - ip_header_len;
+    __u16 new_tcp_len = new_ip_len - ip_header_len;
+    __be16 old_tcp_len_be = bpf_htons(old_tcp_len);
+    __be16 new_tcp_len_be = bpf_htons(new_tcp_len);
+
+    __be16 tcp_csum_old_be;
+    if (bpf_skb_load_bytes(skb, sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check), &tcp_csum_old_be, sizeof(tcp_csum_old_be)) < 0)
+        return -1;
+    //bpf_printk("[UPDATE] Old TCP checksum (on-wire): 0x%04x", bpf_ntohs(tcp_csum_old_be));
+
+    if (bpf_l4_csum_replace(skb, sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check),
+                           old_tcp_len_be, new_tcp_len_be,
+                           BPF_F_PSEUDO_HDR | sizeof(__u16)) < 0)
+        return -1;
+
+    //__be16 tcp_csum_intermediate_be;
+    //if (bpf_skb_load_bytes(skb, sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check), &tcp_csum_intermediate_be, sizeof(tcp_csum_intermediate_be)) < 0)
+    //    return -1;
+    //bpf_printk("[UPDATE] Intermediate TCP checksum: 0x%04x, should be different from old: 0x%04x", bpf_ntohs(tcp_csum_intermediate_be), bpf_ntohs(tcp_csum_old_be));
+    
+    if (bpf_l4_csum_replace(skb, sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check),
+                           0, acc, 0) < 0)
+        return -1;
+    
+    //__be16 tcp_csum_new_be;
+    //if (bpf_skb_load_bytes(skb, sizeof(struct ethhdr) + ip_header_len + offsetof(struct tcphdr, check), &tcp_csum_new_be, sizeof(tcp_csum_new_be)) < 0)
+    //    return -1;
+    //bpf_printk("[UPDATE] New TCP checksum: 0x%04x, should be different from old: 0x%04x and from intermediate: 0x%04x", bpf_ntohs(tcp_csum_new_be), bpf_ntohs(tcp_csum_old_be), bpf_ntohs(tcp_csum_intermediate_be));
+
+        /* 2) IP tot_len: old->new (2 byte on-wire) */
+    //if (csum_diff_16(old_ip_len_be, new_ip_len_be, &acc) < 0)
+    //    return -1;
+//
+    ///* 3) IP checksum: old->new (2 byte on-wire) */
+    //if (csum_diff_16(ip_csum_old_be, ip_csum_new_be, &acc) < 0)
+    //    return -1;
+//
+    ///* 4) TCP checksum: old->new (2 byte on-wire) */
+    //if (csum_diff_16(tcp_csum_old_be, tcp_csum_new_be, &acc) < 0)
+    //    return -1;
+//
+    //if (bpf_csum_update(skb, acc) < 0){
+    //    bpf_printk("[UPDATE] bpf_csum_update not needed");
+    //} else {
+    //    bpf_printk("[UPDATE] bpf_csum_update applied");
+    //}
+
+    
+    
+    return 0;
+}
+
 /* Add keyed BLAKE2s authentication tag to packet */
-static __always_inline __s8 add_hmac(struct __sk_buff *skb, __u8 tcp_payload_offset) {
+static __always_inline __s8 add_hmac(struct __sk_buff *skb, __u8 tcp_payload_offset, __wsum *acc) {
     __u32 new_len;
     __u32 digest[8];
     __u8 secret_key[32] = {
@@ -146,12 +214,16 @@ static __always_inline __s8 add_hmac(struct __sk_buff *skb, __u8 tcp_payload_off
         debug_print("[ADD_HMAC] Failed to store HMAC digest in packet");
         return -1;
     }
+    __s64 t = bpf_csum_diff(NULL, 0, digest, hash_len, *acc);
+    if (t < 0)
+        return TC_ACT_SHOT;
+    *acc = (__wsum)t;
     debug_print("[ADD_HMAC] HMAC successfully added to packet");
     return 1;
 }
 
 /* Remove and verify keyed BLAKE2s authentication tag */
-static __always_inline __s8 remove_hmac(struct __sk_buff *skb, __u8 tcp_payload_offset, __u32 message_start_pos) {
+static __always_inline __s8 remove_hmac(struct __sk_buff *skb, __u8 tcp_payload_offset, __u32 message_start_pos, __wsum *acc) {
     __u8 secret_key[32] = {
         0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
         0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F,
@@ -187,6 +259,11 @@ static __always_inline __s8 remove_hmac(struct __sk_buff *skb, __u8 tcp_payload_
         debug_print("[REMOVE_HMAC] HMAC verification failed - invalid tag");
         return 0;
     }
+
+    __s64 t = bpf_csum_diff(calculated_digest, hash_len, NULL, 0, *acc);
+    if (t < 0)
+        return TC_ACT_SHOT;
+    *acc = (__wsum)t;
     
     debug_print("[REMOVE_HMAC] HMAC verified successfully, need to remove tag");
     return 1;
@@ -215,9 +292,10 @@ int handle_ingress(struct __sk_buff *skb) {
     debug_print("[INGRESS] IP packet: total_len=%d, header_len=%d", ip_tot_old, ip_header_len);
 
     __u8 i;
+    __wsum acc = 0;
     for (i = 0; i < 10; i++) {
         debug_print("[INGRESS] HMAC removal loop iteration %d", i);
-        remove_result = remove_hmac(skb, tcp_payload_offset, skb->len - (32 * (i + 2)));
+        remove_result = remove_hmac(skb, tcp_payload_offset, skb->len - (32 * (i + 2)), &acc);
         debug_print("[INGRESS] remove_hmac result: %d", remove_result);
         
         if (remove_result < 0) {
@@ -247,10 +325,10 @@ int handle_ingress(struct __sk_buff *skb) {
     //} else {
     //    bpf_printk("[INGRESS] Updated skb checksum successfully");
     //}
-    //if (update_len_and_checksums(skb, ip_header_len, ip_tot_old, ip_tot_old - i*hash_len) < 0) {
-    //    debug_print("[INGRESS] Error updating checksums, dropping packet");
-    //    return TC_ACT_SHOT;
-    //}
+    if (update_len_and_checksums(skb, ip_header_len, ip_tot_old, ip_tot_old - i*hash_len, acc) < 0) {
+        debug_print("[INGRESS] Error updating checksums, dropping packet");
+        return TC_ACT_SHOT;
+    }
     bpf_set_hash_invalid(skb);
     bpf_get_hash_recalc(skb);
     debug_print("[INGRESS] Packet processing successful: removed %d HMACs", i);
@@ -282,7 +360,7 @@ int handle_egress(struct __sk_buff *skb) {
     debug_print("[EGRESS] Random HMAC count: %d", random_val);
     random_val += 1; // DEBUG: Garantisce almeno 1 HMAC
     __u8 i;
-
+    __wsum acc = 0;
     for (i = 0; i < 10; i++) {
         if (i >= random_val) {
             debug_print("[EGRESS] Stopping at iteration %d (reached random limit %d)", i, random_val);
@@ -293,7 +371,7 @@ int handle_egress(struct __sk_buff *skb) {
             break;
         }
         debug_print("[EGRESS] Adding HMAC iteration %d", i);
-        hmac_result = add_hmac(skb, tcp_payload_offset);
+        hmac_result = add_hmac(skb, tcp_payload_offset, &acc);
         debug_print("[EGRESS] add_hmac result: %d", hmac_result);
         if (hmac_result < 0) {
             debug_print("[EGRESS] Error in add_hmac, dropping packet");
@@ -311,10 +389,10 @@ int handle_egress(struct __sk_buff *skb) {
     }
 
     debug_print("[EGRESS] Updating checksums for %d added HMACs", i);
-    //if (update_len_and_checksums(skb, ip_header_len, ip_tot_old, ip_tot_old + i*hash_len) < 0) {
-    //    debug_print("[EGRESS] Error updating checksums, dropping packet");
-    //    return TC_ACT_SHOT;
-    //}
+    if (update_len_and_checksums(skb, ip_header_len, ip_tot_old, ip_tot_old + i*hash_len, acc) < 0) {
+        debug_print("[EGRESS] Error updating checksums, dropping packet");
+        return TC_ACT_SHOT;
+    }
 //  
     //if (i > 0) {
     //    __u16 old_len = ip_tot_old;
